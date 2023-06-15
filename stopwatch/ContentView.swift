c4b9923cc108bcba96855bc9e97b4d2686ddc4bb
Shelley Timmins <shelley.timmins@hotmail.co.uk>
2023-06-15 22:29:54 +1000

    Initial UI set up

--- stopwatch/ContentView.swift
+++ stopwatch/ContentView.swift
@@ -9,13 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.accentColor
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Welcome to your stopwatch")
+                    .padding()
+                    .background(Color.mint)
+                    .cornerRadius(10)
+                    .frame(maxWidth: .infinity, alignment: .top)
+                    .font(.system(size: 24, weight: .bold))
+                Spacer()
+                Text("00:00:00")
+                    .padding()
+                    .font(.system(size: 40, weight: .bold))
+                Spacer()
+                HStack {
+                    Text("Start")
+                        .padding()
+                        .font(.system(size: 24, weight: .bold))
+                        .background(Color.green)
+                        .cornerRadius(10)
+                    Text("Reset")
+                        .padding()
+                        .font(.system(size: 24, weight: .bold))
+                        .background(Color.red)
+                        .cornerRadius(10)
+                    Text("Lap")
+                        .padding()
+                        .font(.system(size: 24, weight: .bold))
+                        .background(Color.orange)
+                        .cornerRadius(10)
+                }
+                Spacer()
+            }
         }
-        .padding()
+        
+        
     }
 }
 
