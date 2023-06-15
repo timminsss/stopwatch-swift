1854e7d28de3b827bd44779ba9cae957f9d90855
Shelley Timmins <shelley.timmins@hotmail.co.uk>
2023-06-15 20:17:49 +1000

    Initial Commit

---
+++ stopwatch/stopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  stopwatchApp.swift
+//  stopwatch
+//
+//  Created by Shelley Timmins on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct stopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
