9edcba33858fbb67bcfc68d6eec11c9268145ef7
Shelley Timmins <shelley.timmins@hotmail.co.uk>
2023-06-19 18:50:56 +1000

    timer function now running using picker or buttons

--- stopwatch/TimerView.swift
+++ stopwatch/TimerView.swift
@@ -5,8 +5,14 @@ struct TimerView: View {
     // @State allows us to update the property and UI is updated whenever the state changes
     @State private var showStopwatchView = false
     @State private var runningTime = false
+    // Updated from the picker or from buttons
     @State private var pickerMinutes = 0
     @State private var pickerSeconds = 0
+    // Need to convert the picker mins and secs to time interval
+    @State private var timeRemaining: TimeInterval = 0
+    
+    // Same as stopwatch - publishes each second
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         ZStack {
@@ -14,19 +20,30 @@ struct TimerView: View {
                 .edgesIgnoringSafeArea(.all)
             VStack {
                 Spacer().frame(height: 50)
+                
                 Text("Timer")
                     .padding()
                     .background(Color.mint)
                     .cornerRadius(10)
                     .font(.system(size: 24, weight: .bold))
+                
                 Spacer().frame(height: 50)
                 
-                Text("00:00")
+                Text("\(formatTimeInterval(timeRemaining))")
+                // modifer that picks up any changes from a publisher
+                    .onReceive(timer) { _ in
+                        if runningTime && timeRemaining > 0 {
+                            timeRemaining -= 1
+                        } else {
+                            runningTime = false
+                        }
+                    }
                     .padding()
                     .font(.system(size: 40, weight: .bold))
                 
                 Spacer()
                 
+                // Picker to be able to select custom time
                 Form {
                     HStack{
                         Picker("Minutes", selection: $pickerMinutes) {
@@ -46,11 +63,13 @@ struct TimerView: View {
                     .frame(height: 100)
                 }
                 
-                // update this later to loop through if possible through grid
+                // Update this later to loop through if possible through grid
                 Grid(horizontalSpacing: 24, verticalSpacing: 24) {
                     GridRow {
                         Button {
                             print("1 min")
+                            pickerMinutes = 1
+                            runningTime = true
                         } label: {
                             Text("1 min")
                                 .frame(width: 80, height: 80)
@@ -60,6 +79,8 @@ struct TimerView: View {
                         }
                         Button {
                             print("5 min")
+                            pickerMinutes = 5
+                            runningTime = true
                         } label: {
                             Text("5 min")
                                 .frame(width: 80, height: 80)
@@ -69,6 +90,8 @@ struct TimerView: View {
                         }
                         Button {
                             print("10 min")
+                            pickerMinutes = 10
+                            runningTime = true
                         } label: {
                             Text("10 min")
                                 .frame(width: 80, height: 80)
@@ -80,6 +103,8 @@ struct TimerView: View {
                     GridRow {
                         Button {
                             print("15 min")
+                            pickerMinutes = 15
+                            runningTime = true
                         } label: {
                             Text("15 min")
                                 .frame(width: 80, height: 80)
@@ -89,6 +114,8 @@ struct TimerView: View {
                         }
                         Button {
                             print("20 min")
+                            pickerMinutes = 20
+                            runningTime = true
                         } label: {
                             Text("20 min")
                                 .frame(width: 80, height: 80)
@@ -98,6 +125,8 @@ struct TimerView: View {
                         }
                         Button {
                             print("30 min")
+                            pickerMinutes = 30
+                            runningTime = true
                         } label: {
                             Text("30 min")
                                 .frame(width: 80, height: 80)
@@ -108,6 +137,50 @@ struct TimerView: View {
                     }
                 }
                 
+                
+                HStack {
+                    Button {
+                        print("start")
+                        // Update the timer to the converted time
+                        timeRemaining = convertPickerTimesToTime(pickerMinutes, pickerSeconds)
+                        runningTime = true
+                    } label: {
+                        Text("Start")
+                            .padding()
+                            .font(.headline)
+                            .background(Color.green)
+                            .foregroundColor(.black)
+                            .cornerRadius(10)
+                    }
+                    //                    Might add this later
+                    //                    Button {
+                    //                        print("pause")
+                    //                        runningTime = false
+                    //                    } label: {
+                    //                        Text("Pause")
+                    //                            .padding()
+                    //                            .font(.headline)
+                    //                            .background(Color.green)
+                    //                            .foregroundColor(.black)
+                    //                            .cornerRadius(10)
+                    //                    }
+                    Button {
+                        print("reset")
+                        // Stop the timer and reset
+                        timeRemaining = 0
+                        pickerMinutes = 0
+                        pickerSeconds = 0
+                        runningTime = false
+                    } label: {
+                        Text("Reset")
+                            .padding()
+                            .font(.headline)
+                            .background(Color.red)
+                            .foregroundColor(.black)
+                            .cornerRadius(10)
+                    }
+                    
+                }
                 Button("Show Stopwatch View") {
                     showStopwatchView.toggle()
                 }
@@ -122,6 +195,22 @@ struct TimerView: View {
                 
             }
         }
+    }
+    
+    func convertPickerTimesToTime(_ minutes: Int, _ seconds: Int) -> TimeInterval {
+        // TimeInterval is double therefore output needs to be double
+        let timeIntervalSeconds = Double((minutes * 60) + seconds)
+        timeRemaining = timeIntervalSeconds
+        return TimeInterval(timeIntervalSeconds)
+    }
+    
+    // Using the _ means you don't have to name the parameter when you call it
+    func formatTimeInterval(_ interval: TimeInterval) -> String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.minute, .second]
+        // Adds leading zeros so the format is consistent
+        formatter.zeroFormattingBehavior = .pad
+        return formatter.string(from: interval)!
     }
 }
 
