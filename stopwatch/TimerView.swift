ba53325340cb641184e5ee53d6ce63571d5f0124
Shelley Timmins <shelley.timmins@hotmail.co.uk>
2023-06-19 12:46:24 +1000

    added a picker so you can customise the time in mins and seconds

--- stopwatch/TimerView.swift
+++ stopwatch/TimerView.swift
@@ -5,6 +5,8 @@ struct TimerView: View {
     // @State allows us to update the property and UI is updated whenever the state changes
     @State private var showStopwatchView = false
     @State private var runningTime = false
+    @State private var pickerMinutes = 0
+    @State private var pickerSeconds = 0
     
     var body: some View {
         ZStack {
@@ -16,10 +18,35 @@ struct TimerView: View {
                     .padding()
                     .background(Color.mint)
                     .cornerRadius(10)
-                    .frame(maxWidth: .infinity, alignment: .top)
                     .font(.system(size: 24, weight: .bold))
+                Spacer().frame(height: 50)
+                
+                Text("00:00")
+                    .padding()
+                    .font(.system(size: 40, weight: .bold))
+                
                 Spacer()
                 
+                Form {
+                    HStack{
+                        Picker("Minutes", selection: $pickerMinutes) {
+                            ForEach(0..<60) { minute in
+                                Text("\(minute) mins")
+                            }
+                        }
+                        .pickerStyle(WheelPickerStyle())
+                        
+                        Picker("Seconds", selection: $pickerSeconds) {
+                            ForEach(0..<60) { second in
+                                Text("\(second) secs")
+                            }
+                        }
+                        .pickerStyle(WheelPickerStyle())
+                    }
+                    .frame(height: 100)
+                }
+                
+                // update this later to loop through if possible through grid
                 Grid(horizontalSpacing: 24, verticalSpacing: 24) {
                     GridRow {
                         Button {
@@ -27,27 +54,27 @@ struct TimerView: View {
                         } label: {
                             Text("1 min")
                                 .frame(width: 80, height: 80)
-                                                .foregroundColor(Color.white)
-                                                .background(Color.black)
-                                                .clipShape(Circle())
+                                .foregroundColor(Color.white)
+                                .background(Color.black)
+                                .clipShape(Circle())
                         }
                         Button {
                             print("5 min")
                         } label: {
                             Text("5 min")
                                 .frame(width: 80, height: 80)
-                                                .foregroundColor(Color.white)
-                                                .background(Color.black)
-                                                .clipShape(Circle())
+                                .foregroundColor(Color.white)
+                                .background(Color.black)
+                                .clipShape(Circle())
                         }
                         Button {
                             print("10 min")
                         } label: {
                             Text("10 min")
                                 .frame(width: 80, height: 80)
-                                                .foregroundColor(Color.white)
-                                                .background(Color.black)
-                                                .clipShape(Circle())
+                                .foregroundColor(Color.white)
+                                .background(Color.black)
+                                .clipShape(Circle())
                         }
                     }
                     GridRow {
@@ -56,27 +83,27 @@ struct TimerView: View {
                         } label: {
                             Text("15 min")
                                 .frame(width: 80, height: 80)
-                                                .foregroundColor(Color.white)
-                                                .background(Color.black)
-                                                .clipShape(Circle())
+                                .foregroundColor(Color.white)
+                                .background(Color.black)
+                                .clipShape(Circle())
                         }
                         Button {
                             print("20 min")
                         } label: {
                             Text("20 min")
                                 .frame(width: 80, height: 80)
-                                                .foregroundColor(Color.white)
-                                                .background(Color.black)
-                                                .clipShape(Circle())
+                                .foregroundColor(Color.white)
+                                .background(Color.black)
+                                .clipShape(Circle())
                         }
                         Button {
                             print("30 min")
                         } label: {
                             Text("30 min")
                                 .frame(width: 80, height: 80)
-                                                .foregroundColor(Color.white)
-                                                .background(Color.black)
-                                                .clipShape(Circle())
+                                .foregroundColor(Color.white)
+                                .background(Color.black)
+                                .clipShape(Circle())
                         }
                     }
                 }
@@ -93,23 +120,8 @@ struct TimerView: View {
                 .cornerRadius(10)
                 .font(.headline)
                 
-                
             }
         }
-    }
-    
-    // using the _ means you don't have to name the parameter when you call it
-    func formatTimeInterval(_ interval: TimeInterval) -> String {
-        // formatter doesn't suppoer ms so need to calc manually
-        // can't use % on decimals so need truncatingRemainder
-        let milliseconds = Int((interval.truncatingRemainder(dividingBy: 1)) * 100)
-        let formatter = DateComponentsFormatter()
-        formatter.allowedUnits = [.hour, .minute, .second]
-        // adds leading zeros so the format is consistent
-        formatter.zeroFormattingBehavior = .pad
-        
-        // return the time plus manually calc ms
-        return formatter.string(from: interval)! + String(format: ":%02d", milliseconds)
     }
 }
 
